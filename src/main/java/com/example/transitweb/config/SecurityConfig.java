64a78f5746dbcc03603e411fe09481e88b0bbdea
Edy Tola <jaircitola@gmail.com>
2025-10-28 15:05:53 -0500

    Conexion con mysql

--- src/main/java/com/example/transitweb/config/SecurityConfig.java
+++ src/main/java/com/example/transitweb/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -31,6 +32,7 @@ import java.util.Arrays;
 public class SecurityConfig {
 
     @Autowired
+    @Lazy
     private UsuarioService usuarioService; // El servicio que implementa UserDetailsService
 
     /**
